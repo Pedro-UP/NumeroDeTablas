06b517c933749f1119e96d48069d3a97e718fb50
Pedro-UP <pedrocazorla03@gmail.com>
2022-04-22 22:50:50 -0500

    Elegir numero de tablas de multiplicar

---
+++ NumeroDeTablas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TablasdeMultiplicar
+{
+    class Program
+    {
+        static int tabla, n;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el Numero de Tablas");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <=n; i++)
+            {
+                Console.WriteLine("Tabla del :" + i);
+
+                
+
+                for (int x = 1; x <= 10; x++)
+                {
+
+                    tabla = x * i;
+
+                    Console.WriteLine("{0} * {1} es :{2}", i, x, tabla);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
